6bd447fd83a2ca6b89e1ca3d00ac12addc7e7b1c
Ahmad-Faqehi <ahmadpsn7@gmail.com>
2021-08-22 20:30:39 +0300

    Add link to title of blog

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,7 +13,7 @@
                         </router-link>
                     </div>
                     <div class="header-top-text">
-                        <p> Ahmad Blog </p>
+                       <router-link :to="{ name: 'Home'}"> <p>Ahmad Blog</p> </router-link>
                     </div>
                     <nav class="header-top-nav">
                         <ul>
