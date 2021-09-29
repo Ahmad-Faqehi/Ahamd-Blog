856373e02f160f66afd0443804dd79124c58c9d0
Ahmad-Faqehi <ahmadpsn7@gmail.com>
2021-09-29 07:56:53 +0300

    Testing env in prod

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let api_url = process.env.VUE_APP_BASE_API
-
+console.log(process.env.VUE_APP_BASE_API)
 if(!api_url){ api_url = 'http://api.iepes.site/api'}
 console.log(api_url)
 const Api = axios.create({
